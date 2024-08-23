52d7dd8e7a3627af30e35330dea2388e07012d77
Milena <mirenahayakawa@gmail.com>
2024-08-23 16:29:13 -0300

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all/min.css';
+
+const App = () => {
+    return (
+        <div className='container border mt-2 py-5'>
+	<div className='row'>
+	<div className='col-12'>
+	<h1 className='display-5 text-center'
+
+    
+
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
