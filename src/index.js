0a60e3ea76ce53ac3fb458f32316b1b89bb8b3e0
Milena Sayuri Hayakawa <mirenahayakawa@gmail.com>
2024-08-30 16:37:37 -0300

    exercicio com props encerrada

--- src/index.js
+++ src/index.js
@@ -1,50 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import Pedido from './pedido'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pedido from "./Pedido.js";
+import Cartao from "./Cartao.js";
+import FeedBack from "./FeedBack.js";
 
 const App = () => {
-    return (
-        <div className='container border mt-2 py-5'>
-            <div className='row'>
-                <div className='col-12'>
-                    <h1 className='display-5 text-center'>Seus pedidos</h1>
-                </div>
-            </div>
-            <div className='row'>
-                <div className="col-sm-12 col-lg-12 col-xl-4 border py-5">
-                    <Pedido
-                        data='22/04/2024'
-                        icone='fa-solid fa-hard-drive fa-shake fa-3x'
-                        titulo='SSD'
-                        descricao='SSD Kingston'
-                    />
-                </div>
-
-                <div className="col-sm-12 col-lg-12 col-xl-4 border py-5">
-                    <Pedido
-                        data='24/06/2024'
-                        icone='fa-solid fa-computer-mouse fa-beat fa-3x'
-                        titulo='Mouse'
-                        descricao='Mouse Razer Viper Mini'
-                    />
-
-                </div>
-                <div className="col-sm-12 col-lg-12 col-xl-4 border py-5">
-                    <Pedido
-                        data='15/09/2024'
-                        icone='fa-solid fa-headset fa-fade fa-3x'
-                        titulo='HeadSet'
-                        descricao='HeadSet Logitech G Pro X 2'
-                    />
-                </div>
-            </div>
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Ainda não chegou'
+  const funcaoOK = () => alert('Agradecemos a confirmação')
+  const funcaoNOK = () => alert('Verificaremos o ocorrido')
+  const componenteFeedBack = 
+    <FeedBack textoOK={textoOK} textoNOK={textoNOK} funcaoOK={funcaoOK} funcaoNOK={funcaoNOK}/>
+        
+  return (
+    <div className="container border mt-2 py-5">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="display-5 text-center">Seus pedidos</h1>
         </div>
-    )
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-lg-12 col-xl-4 border py-5">
+          <Cartao cabecalho="22/04/2024">
+            <Pedido
+              icone="fa-solid fa-hard-drive fa-shake fa-3x"
+              titulo="SSD"
+              descricao="SSD Kingston"
+            />
+            {componenteFeedBack}
+          </Cartao>
+        </div>
+
+        <div className="col-sm-12 col-lg-12 col-xl-4 border py-5">
+          <Cartao cabecalho="24/06/2024">
+            <Pedido
+              icone="fa-solid fa-computer-mouse fa-beat fa-3x"
+              titulo="Mouse"
+              descricao="Mouse Razer Viper Mini"
+            />
+             {componenteFeedBack}
+          </Cartao>
+        </div>
+        <div className="col-sm-12 col-lg-12 col-xl-4 border py-5">
+          <Cartao cabecalho="15/09/2024">
+            <Pedido
+              icone="fa-solid fa-headset fa-fade fa-3x"
+              titulo="HeadSet"
+              descricao="HeadSet Logitech G Pro X 2"
+            />
+             {componenteFeedBack}
+          </Cartao>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
